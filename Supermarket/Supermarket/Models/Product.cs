c9bcec586ad1005be7726b8b90f029f4a3ae29e7
FilipPopescu <PopescuFilip@users.noreply.github.com>
2024-05-11 23:36:10 +0300

    switched to .NET

---
+++ Supermarket/Supermarket/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Barcode { get; set; }
+        public string Title { get; set; }
+    }
+}
