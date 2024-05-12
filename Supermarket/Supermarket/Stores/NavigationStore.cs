ceecb7a73416fe2968f54a260a01ed8a109ea0c4
FilipPopescu <PopescuFilip@users.noreply.github.com>
2024-05-12 18:54:15 +0300

    added base classes

---
+++ Supermarket/Supermarket/Stores/NavigationStore.cs
@@ -0,0 +1,29 @@
+﻿using Checkers.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkers.Stores
+{
+    public class NavigationStore
+    {
+        private ViewModelBase? _currentViewModel;
+        public ViewModelBase? CurrentViewModel
+        {
+            get => _currentViewModel;
+            set
+            {
+                _currentViewModel = value;
+                OnCurrentViewModelChanged();
+            }
+        }
+
+        public event Action? CurrentViewModelChanged;
+        private void OnCurrentViewModelChanged()
+        {
+            CurrentViewModelChanged?.Invoke();
+        }
+    }
+}
