ad4f8236e2b4e6ce1df7fc11266bae1d07a2cd14
FilipPopescu <PopescuFilip@users.noreply.github.com>
2024-05-17 12:56:45 +0300

    ViewModelFactory interface and implementation for LoginViewModelFactory

---
+++ Supermarket/Supermarket/ViewModels/Factories/LoginViewModelFactory.cs
@@ -0,0 +1,22 @@
+﻿using Supermarket.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket.ViewModels.Factories
+{
+    public class LoginViewModelFactory : IViewModelFactory<LoginViewModel>
+    {
+        private readonly AuthenticationService _authenticationService;
+        public LoginViewModelFactory(AuthenticationService authenticationService) 
+        {
+            _authenticationService = authenticationService;
+        }
+        public LoginViewModel CreateViewModel()
+        {
+            return new LoginViewModel(_authenticationService);
+        }
+    }
+}
